3c97a66fb250e07fc9096d917c767e52874be1f0
Bishal Roy <bishalroy15jan@gmail.com>
2025-01-19 01:04:56 +0530

    fix build errors

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['cloudflare-ipfs.com', 'loremflickr.com'],
-  },
-}
-
-module.exports = nextConfig 
--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   images: {
-    domains: ['cloudflare-ipfs.com'],
+    domains: ['cloudflare-ipfs.com', 'loremflickr.com'],
   },
   typescript: {
+    // Uncomment the following line if you want to ignore TypeScript errors during build
     ignoreBuildErrors: true,
   },
   eslint: {
+    // Uncomment the following line if you want to ignore ESLint errors during build
     ignoreDuringBuilds: true,
   },
 };
